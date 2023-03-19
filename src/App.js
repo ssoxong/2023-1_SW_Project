5ce42f343081d1d5718af42bd0f5b0a264b72d4a
ssoxong <ssoxong@naver.com>
2023-03-19 15:25:52 +0900

    react_teq_14.2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import Users from './Users';
+import {useState} from 'react';
+import axios from 'axios';
 
-function App() {
-  return <Users />;
-}
+const App=()=>{
+  const[data, setData] = useState(null);
+  const onClick=()=>{
+    axios.get("https://jsonplaceholder.typicode.com/todos/1").then(response=>{
+      setData(response.data);
+    });
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data&&<textarea rows={7} value={JSON.stringify(data,null,2)} readOnly={true}/>}
+      </div>
+  
+  );
+};
 
 export default App;
