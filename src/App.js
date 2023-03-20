13910896bfb39e1f798b3f201b958b4ece2317aa
ssoxong <ssoxong@naver.com>
2023-03-20 16:10:58 +0900

    news api test

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import axios from 'axios';
 
 const App=()=>{
   const[data, setData] = useState(null);
+  
   const onClick=()=>{
-    axios.get("https://jsonplaceholder.typicode.com/todos/1").then(response=>{
+    axios.get("https://newsapi.org/v2/top-headlines?country=kr&apikey=2275c1ed4cf8447582d46ada9859831f",/*{
+      headers:{
+        Authorizaion:"Xkj0WKwCnecDGF4RCVJfNosJtVSBEU23EivB"
+      }
+    }*/).then(response=>{
       setData(response.data);
     });
-  };
+
+  return;
+};
 
   return (
     <div>
